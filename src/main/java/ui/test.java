72252dbd05905c9aa39e1dc2cf58b0405a72b5a5
j7ur8 <j7ur8@qq.com>
2022-04-08 00:50:55 +0800

    重构

--- src/main/java/ui/test.java
+++ src/main/java/ui/test.java
@@ -5,9 +5,7 @@ import burp.lib.Helper;
 import java.awt.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -21,46 +19,71 @@ public class test {
         String lineBuffer = "";
         String fileLocation = "C:\\Users\\J7ur8\\Desktop\\Jrcet.coffee";
         ArrayList<String> lineList = new ArrayList<>();
-        ArrayList<String> renderedLineList1 = new ArrayList<>();
-        ArrayList<String> renderedLineList2 = new ArrayList<>();
+        ArrayList<String> renderedLineList = new ArrayList<>();
         BufferedReader fileBuffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileLocation), StandardCharsets.UTF_8));
 
         while ((lineBuffer = fileBuffer.readLine()) != null) {
             lineList.add(lineBuffer);
         }
 
-        System.out.println(lineList.size());
-        for(int i=0; i<lineList.size()-1;i++){
+//        System.out.println(lineList.size()); //├─   └─ │
+
+        for(int i=0; i<=lineList.size()-1;i++){
+
             int j = i+1;
-            int nowLineTabCount = CharCount(lineList.get(i), "\t");
-            //├─   └─ │
-            while(j<lineList.size()-1) {
-                int tmpTabCount = CharCount(lineList.get(j),"\t");
+            int nowLineTabCount = CharCount(lineList.get(i), " ");
+
+            if(i==lineList.size()-1){
+                renderedLineList.add(Pattern.compile(" ([^ ]*)$").matcher(lineList.get(i)).replaceAll(" └─$1"));
+            }
+
+            while(j<lineList.size()) {
+                int tmpTabCount = CharCount(lineList.get(j)," ");
+                if(nowLineTabCount < tmpTabCount &&  j==lineList.size()-1){
+                    renderedLineList.add(Pattern.compile(" ([^ ]*)$").matcher(lineList.get(i)).replaceAll(" └─$1"));
+                    break;
+                }
                 if(nowLineTabCount > tmpTabCount) {
-                    renderedLineList1.add(Pattern.compile("\t([^\t]*)$").matcher(lineList.get(i)).replaceAll("\t└─$1"));
+                    renderedLineList.add(Pattern.compile(" ([^ ]*)$").matcher(lineList.get(i)).replaceAll(" └─$1"));
                     break;
                 }
 
                 if( nowLineTabCount == tmpTabCount ) {
-//                    if(j==i+1){
-                    renderedLineList1.add(Pattern.compile("\t([^\t]*)$").matcher(lineList.get(i)).replaceAll("\t├─$1"));
+                    renderedLineList.add(Pattern.compile(" ([^ ]*)$").matcher(lineList.get(i)).replaceAll(" ├─$1"));
                     break;
-//                    }
-//                    renderedLineList1.add(Pattern.compile("\t([^\t]*)$").matcher(lineList.get(i)).replaceAll("\t└─$1"));
-//                    break;
                 }
                 j++;
-
             }
         }
 
-        for (int i=0; i<renderedLineList1.size()-1;i++){
+        for (int i=0; i<renderedLineList.size()-1;i++){
+
+            int j = i+1;
+            int pos = renderedLineList.get(i).indexOf("├");
+
+            while (j<renderedLineList.size()-1){
+
+                if(j==1 || renderedLineList.get(j).indexOf("│") == pos || renderedLineList.get(j).indexOf("└") == pos){
+                    break;
+                }
+
+                if(renderedLineList.get(j).indexOf("├") == pos){
+                    j=j+1;
+                    continue;
+                }
+
+                StringBuilder lineBuilder = new StringBuilder(renderedLineList.get(j));
+                if(!Objects.equals(renderedLineList.get(j), " ")){
+                    renderedLineList.set(j,lineBuilder.replace(pos,pos+1,"│").toString());
+                }
+                j=j+1;
+            }
 
         }
-        for(String c:renderedLineList1){
-            System.out.println(c);
-        }
 
+        for(String line:renderedLineList) {
+            System.out.println(line);
+        }
     }
 
     public static int CharCount(String srcText, String findText) {
